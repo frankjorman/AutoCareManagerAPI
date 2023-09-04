fa775e730166602c6c55d43d6a274e8ac1da310e
frank <fjachavezc@gmail.com>
2023-09-04 01:46:29 -0500

    Agregar archivos de proyecto.

---
+++ AutoCareManagerAPI/Entities/Mecanico.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoCareManagerAPI.Entities
+{
+    public class Mecanico
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string correoElectronico { get; set; }
+        public string cargo { get; set; }
+        public string especializacion { get; set; }
+    }
+}
---
+++ AutoCareManagerAPI/Entities/Reparaciones  .cs
@@ -0,0 +1,10 @@
+﻿namespace AutoCareManagerAPI.Entities
+{
+    public class Reparaciones
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public string descripcion { get; set; }
+        public double costo { get; set; }
+    }
+}
