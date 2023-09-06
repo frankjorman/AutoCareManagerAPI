9321c82764a13389d5fa823832b883040e1a3679
frank <fjachavezc@gmail.com>
2023-09-06 01:19:49 -0500

    Controlador Mecanico

---
+++ AutoCareManagerAPI/Data/AutoCareManagerAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using AutoCareManagerAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoCareManagerAPI.Data
+{
+    public class AutoCareManagerAPIContext:DbContext
+    {
+        public AutoCareManagerAPIContext(DbContextOptions options) :base(options) { }
+
+        public DbSet<Mecanico> Mecanico { get; set; }
+        public DbSet<Reparaciones> Reparaciones { get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
---
+++ AutoCareManagerAPI/Entities/Vehiculo.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoCareManagerAPI.Entities
+{
+    public class Vehiculo
+    {
+        public int id { get; set; }
+        public string placa { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public int AnioFabricacion { get; set; }
+        public int idCliente { get; set; }
+
+    }
+}
